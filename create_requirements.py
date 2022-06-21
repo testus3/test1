e68e6143417dc39034b89dc9fa5d382e32623e52
testus3 <107894841+testus3@users.noreply.github.com>
2022-06-21 01:48:32 -0500

    Create create_requirements.py

---
+++ create_requirements.py
@@ -0,0 +1,28 @@
+import re
+import os
+
+folder = '.'
+
+files = [f for f in os.listdir(folder) if f.endswith(".py")]
+
+imports = []
+for file in files:
+    with open(os.path.join(folder, file), mode="r") as f:
+        lines = f.read()
+        result = re.findall(r"(?<!from)import (\w+)[\n.]|from\s+(\w+)\s+import", lines)
+        for imp in result:
+            for i in imp:
+                if len(i):
+                    if i not in imports:
+                        imports.append(i)
+                        
+list_to_remove = ['os','re']
+final_list = list(set(imports) - set(list_to_remove))
+print(final_list)
+#print(len(final_list))
+file = open("requirements.txt", "w+")
+for imported in final_list:
+    content = str(imported)
+    file.write(f'{content}\n')
+
+file.close()
