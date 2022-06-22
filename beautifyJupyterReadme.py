cd6f695846301a390dd2a0b32dda6057cb038dd7
testus3 <107894841+testus3@users.noreply.github.com>
2022-06-22 03:11:08 -0500

    Update beautifyJupyterReadme.py

--- beautifyJupyterReadme.py
+++ beautifyJupyterReadme.py
@@ -7,11 +7,13 @@ files = [f for f in os.listdir(folder) if f.endswith(".md")]
 
 urlStringStart = "<img src=\"https://latex.codecogs.com/svg.image?{\\large\\color{Blue}\\pmb{"
 urlStringEnd = "}\" align=\"center\">"
-imports = []
 
 
 #pattern1dollar = "[^0-9a-zA-Z?]\$(.*)\$[^\$0-9a-zA-Z?]"
 pattern1dollar = "[^\$0-9a-zA-Z?]\$(.*?)\$[^\$0-9?]"
+#patternDollarDollar = "(?<=\$\$)[\S\s]*(?=\$\$)"
+#patternDollarDollar = "(?<=\$\$)[\S\s]*(?=\n\$\$)"
+patternDollarDollar = "(?s)(?<=\$\$)(.*?)(?=\$\$)"
 
 
 #Fix equations and add bold & style
@@ -21,17 +23,39 @@ for file in files:
         
         # Delete bolds from jupyter
         lines = lines.replace("**", "")
-        lines = lines.replace("$$", "$$\n")
-        #lines = lines.replace("$$", "\n$$\n")
+        #lines = lines.replace("\$\$(.*)\$\$", "\n\$\$(.*)\n")
+        #lines = re.sub('\$\$(.*)\$\$', '\n\$\$(.*)\$\$\n', lines)
         # Fix equation title list(filter(None, ))
         
-        arrayDollars = list(filter(None,re.findall(pattern1dollar, lines) ))
+        arrayDollars = list(filter(None,re.findall(pattern1dollar, lines)))
+        arrayDollarsDollars = list(filter(None,re.findall(patternDollarDollar, lines)))
+        
         #print(arrayDollars)
         
         #Counting number of ocurrences of $$
         numberOfOcurrences = len(arrayDollars)
         #print(numberOfOcurrences)
         #print(re.findall(pattern1dollar, lines))
+        
+        numberOfOcurrencesDD = len(arrayDollarsDollars)
+        substringsDD = arrayDollarsDollars
+        #print(substringsDD)
+        #print(len(substringsDD))
+        for i in range(numberOfOcurrencesDD):
+            # totalsubstringDD = "$$" + substringsDD[i] +"$$" # todo string $..$
+            # #print(totalsubstringDD)
+            # newsubstringDD =  "\n$$" +  substringsDD[i] + "$$\n"
+            # with open(os.path.join(folder, file), mode="w") as f:
+                # lines = lines.replace(totalsubstringDD, newsubstringDD)
+                # f.write(lines)
+            if(i <= (numberOfOcurrencesDD +1)*0.5):
+                totalsubstringDD = "$$" + substringsDD[i] +"$$" # todo string $..$
+                #print(totalsubstringDD)
+                newsubstringDD =  "\n$$" +  substringsDD[i] + "$$\n"
+                with open(os.path.join(folder, file), mode="w") as f:
+                    lines = lines.replace(totalsubstringDD, newsubstringDD)
+                    f.write(lines)
+                i=i+2;
         
         substrings= arrayDollars
 
